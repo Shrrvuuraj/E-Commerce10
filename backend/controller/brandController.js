7bd96178d38a7868b2c441c792d65f3c567d61d3
Shravan Rajput <shrrvvuurajput@gmail.com>
2025-08-30 00:58:09 +0530

    brand api completed

--- backend/controller/brandController.js
+++ backend/controller/brandController.js
@@ -1,3 +1,70 @@
-export const brandController = (req, res) => {
-     res.send("brandcontroller")
+import Brand from "../model/brand.js";
+
+export const getAllBrand = async (req, res) => {
+  try {
+    const brands = await Brand.find();
+    res.status(200).json(brands);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const postBrand = async (req, res) => {
+  try {
+    const brand = await Brand.create(req.body);
+    res.status(201).json(brand);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getBrand = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const brand = await Brand.findById(id);
+
+    if (!brand) {
+      return res.status(404).json({ message: "Brand not found" });
+    }
+
+    res.status(200).json(brand);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const putBrand = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const body = req.body;
+
+    const brand = await Brand.findByIdAndUpdate(id, body, { new: true });
+
+    if (!brand) {
+      return res.status(404).json({ message: "Brand not found" });
+    }
+
+    res.status(200).json(brand);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteBrand = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const brand = await Brand.findByIdAndDelete(id);
+
+    if (!brand) {
+      return res.status(404).json({ message: "Brand not found" });
+    }
+
+    res.status(200).json({
+      message: "Brand deleted successfully",
+      brand,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
--- backend/routes/brandRoute.js
+++ backend/routes/brandRoute.js
@@ -1,6 +1,24 @@
 import e from "express";
-const router=e.Router();
-import {brandController} from "../controller/brandController.js"
+const router = e.Router();
+import {
+  getAllBrand,
+  getBrand,
+  putBrand,
+  deleteBrand,
+  postBrand,
+} from "../controller/brandController.js";
 
-router.get("/",brandController)
-export default router
+
+router
+     .route("/")
+     .get(getAllBrand)
+     .post(postBrand);
+
+
+     router
+     .route("/:id")
+     .get(getBrand)
+     .put(putBrand)
+     .delete(deleteBrand);
+
+export default router;
